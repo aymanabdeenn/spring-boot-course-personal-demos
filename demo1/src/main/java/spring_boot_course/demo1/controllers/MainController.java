48fbe5c3fa276dccbf424502e231e92e0f831c99
aymanabdeenn <151556377+aymanabdeenn@users.noreply.github.com>
2025-07-25 16:50:31 +0300

    Made some changes.

---
+++ demo1/src/main/java/spring_boot_course/demo1/controllers/MainController.java
@@ -0,0 +1,16 @@
+package spring_boot_course.demo1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class MainController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, world!";
+    }
+
+}
