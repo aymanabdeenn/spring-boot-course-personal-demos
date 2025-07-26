fcbcc3b0c4d8378b15f62a8479237c7987b75911
aymanabdeenn <151556377+aymanabdeenn@users.noreply.github.com>
2025-07-26 10:23:16 +0300

    Added some changes.

--- demo1/src/main/java/spring_boot_course/demo1/controllers/MainController.java
+++ demo1/src/main/java/spring_boot_course/demo1/controllers/MainController.java
@@ -1,12 +1,18 @@
 package spring_boot_course.demo1.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/main")
 public class MainController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/hello")
     public String hello(){
@@ -16,6 +22,11 @@ public class MainController {
     @GetMapping("/luck")
     public String goodLuck(){
         return "Good Luck!";
+    }
+
+    @GetMapping("/mouse")
+    public String mouseInfo(){
+        return "Coach name: " + coachName + " / Team name: " + teamName;
     }
 
 }
